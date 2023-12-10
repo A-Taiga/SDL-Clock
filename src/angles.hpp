e1fad54e681eb22409b5668af1453429cd327996
Anthony Polka <anthonypolka@gmail.com>
2023-12-09 22:36:07 -0600

    removed public specifier

--- src/angles.hpp
+++ src/angles.hpp
@@ -22,7 +22,6 @@ namespace angles
 	template<AngleType T>
 	struct Radians
 	{
-		public:
 		T val;
 		constexpr Radians() = default;
 		constexpr Radians(T v);
@@ -46,7 +45,6 @@ namespace angles
 	template<AngleType T>
 	struct Degrees
 	{
-		public:
 		T val;
 
 		constexpr Degrees() = default;
@@ -74,7 +72,6 @@ namespace angles
 	template<AngleType T>
 	struct ArcMinutes
 	{
-		public:
 		T val;
 
 		constexpr ArcMinutes() = default;
